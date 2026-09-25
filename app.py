c25043fb92b74e9ba9ea90c7199dede9e53cdfdd
Seitkasymovn-design <Seitkasymov.n@mail.ru>
2026-09-25 12:30:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if api_key:
         with st.chat_message("assistant"):
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-3.8-flash",
                     contents=prompt,
                     config={'system_instruction': system_instruction}
                 )
